ebb0f597ca6bd1d879078a7861885bc4904ec37c
jbiloso <jbiloso@up.edu.ph>
2025-04-15 02:36:13 +0800

    Exercise 2.3: Course information step 8 - calculate the sum of exercises with the array method reduce

--- courseinfo/src/components/Total.jsx
+++ courseinfo/src/components/Total.jsx
@@ -1,8 +1,5 @@
 const Total = ({ parts }) => {
-    // console.log(parts) 
-
     const exercises = parts.map(part => part.exercises)
-    // console.log(exercises)
     const total = exercises.reduce((total, exercise) => total + exercise, 0)
     return (
         <div>
