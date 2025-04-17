19b66a9688fc427dddb3b268d23783dd2ae4d001
jbiloso <jbiloso@up.edu.ph>
2025-04-18 07:29:24 +0800

    Made a module for country info

--- countries/src/App.jsx
+++ countries/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import Country from './components/Country'
 
 const App = () => { 
   const [ countries, setCountries] = useState(null)
@@ -34,6 +35,7 @@ const App = () => {
     }
     // console.log('countriesSearched.length : ',countriesSearched.length)
   } 
+
   if(!visibleCountries){ //if there's no country being searched, or none matches the search
     return ( 
       <div>
@@ -61,33 +63,11 @@ const App = () => {
     )
   }
   if(visibleCountries.length == 1){ //if only one country matches the search, return the info
-    // the .languages of the country is an object with unknown keys 
-    // console.log(visibleCountries[0].languages)
-    // console.log(Object.values(visibleCountries[0].languages))
-
-    //getting the values of the object languages when we do not know the keys
-    const languages = Object.values(visibleCountries[0].languages)
-
-    //we return the capital, area, languages, and the map
-
     return(
       <div>
         <>find countries </>
         <input type='text' onChange={(event)=> handleSearchChange(event)} placeholder='Search...'></input>
-        <div>
-          <h1>{visibleCountries[0].name.official}</h1>
-        </div>
-        <div>{`Capital: ${visibleCountries[0].capital}`}</div>
-        <div>{`Area: ${visibleCountries[0].area}`}</div>
-        <div>
-          <h2>Languages</h2>
-        </div>
-        <div>
-          {languages.map(lang => <li key={lang}>{lang}</li>)}
-        </div>
-        <div>
-          <img src ={visibleCountries[0].flags.png} ></img>
-        </div>
+        <Country country={visibleCountries[0]}></Country>
       </div>
     )
   }
---
+++ countries/src/components/Country.jsx
@@ -0,0 +1,29 @@
+const Country = ({ country }) => {
+    // the .languages of the country is an object with unknown keys 
+    // console.log(visibleCountries[0].languages)
+    // console.log(Object.values(visibleCountries[0].languages))
+
+    //getting the values of the object languages when we do not know the keys
+    const languages = Object.values(country.languages)
+
+    //we return the capital, area, languages, and the map
+    return (
+        <div>
+            <div>
+            <h1>{country.name.official}</h1>
+            </div>
+            <div>{`Capital: ${country.capital}`}</div>
+            <div>{`Area: ${country.area}`}</div>
+            <div>
+            <h2>Languages</h2>
+            </div>
+            <div>
+            {languages.map(lang => <li key={lang}>{lang}</li>)}
+            </div>
+            <div>
+            <img src ={country.flags.png} ></img>
+            </div>
+        </div>
+    )
+}
+export default Country
